50babf9f618ccaee401a7a8d795c5777eacec700
duc77rma <duc7rma@gmail.com>
2024-02-27 03:31:16 +0700

    API login

---
+++ pages/api/posts/index.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data =
+  | {
+      data: any[]
+      pagination: any
+    }
+  | { name: string }
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  if (req.method !== 'GET') {
+    return res.status(404).json({ name: 'method not supported' })
+  }
+
+  const baseUrl = 'https://js-post-api.herokuapp.com/api';
+
+  const response = await fetch(baseUrl + '/posts?_page=1&_limit=10');
+
+  console.log("response: ", response );
+  
+  const responseJSON = await response.json();
+
+  res.status(200).json(responseJSON)
+};
